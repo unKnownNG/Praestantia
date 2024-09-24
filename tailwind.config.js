5c79792f7825ab93736dc1eb5ecd2f241ea7c4e2
unknownng <mohammeddaiyaan2005@gmail.com>
2024-09-24 19:59:47 +0530

    Pushing everything

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      
+        fontFamily: {
+          custom: ['"MyCustomFont"', 'sans-serif'],
+        },
+      
+    },
+  },
+  plugins: [],
+};
